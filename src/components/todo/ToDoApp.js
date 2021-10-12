ff4fa7b348724bcc4d3cf7a4ea3b330504b188a4
Mark Abou-Ghayda (Ghaida) <mark.ag@c02zq2qrmd6r.lan>
2021-10-12 15:03:42 -0400

    added in first input elements for form

--- src/components/todo/ToDoApp.js
+++ src/components/todo/ToDoApp.js
@@ -4,9 +4,20 @@ class ToDoApp extends Component{
     render(){
     return(
         <>
-            Hello
+            My To Do Application
         </>
     )
 }
+}
+
+class LoginComponent extends Component {
+    render(){
+        return(
+        <>
+            Username: <input type="text" name="username"/>
+            Password: <input type='password' name="password"/> 
+        </>
+        )
+    }
 }
 export default ToDoApp;
